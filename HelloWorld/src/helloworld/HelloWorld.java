4e0809d9590661dff8cdfa1a2f30df2df4bfad4a
TMRM <tyrie.moss@gmail.com>
2020-05-19 18:29:28 -0400

    Initial commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @AUTHOR T
+ * @CONTRIBUTOR TMRM
+ * @PROJECT ${Java App - HelloWorld}
+ * @VERSION 1.0
+ * @SINCE ${05-12-2020}
+ */
+public class HelloWorld {
+
+    /**
+     * Printing Greetings
+     */
+    public static void main(String[] args) {
+      System.out.println ("Hello World!"); // println - prints the characters in the double quotes ("") and then goes to the next line
+      System.out.print ("Goodbye!"); // print - simply prints the characters in the double quotes ("")
+      System.out.print ("\nSee You Soon!"); // \n - Carriage Return
+      System.out.print ("\tSee You Tomorrow!"); // \t - adds several spaces
+      // // - Single-line comment
+      // */ - Multi-line comment
+    }
+    
+}
