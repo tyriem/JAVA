afad1f1942865ae39cb3ed40908f1ef311f0bbaf
TMRM <tyrie.moss@gmail.com>
2020-05-19 18:29:50 -0400

    Initial commit

---
+++ DiplayOut/src/diplayout/DiplayOut.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package diplayout;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intro To Java - DisplayOoutput
+ * @version: 1.0
+ * @since: 05-13-2020
+ */
+public class DiplayOut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Tyrie M.R. Moss \n \t Course #1: Student Success \n \t Course #2 Intro to Python \n \t Course #3 Intro to Java");
+    }
+
+}
