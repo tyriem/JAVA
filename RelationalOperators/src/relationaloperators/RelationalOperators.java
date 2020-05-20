8675c1a834594aa7ae2f7ec4b76231cb5ee894a0
TMRM <tyrie.moss@gmail.com>
2020-05-20 16:06:43 -0400

    Initial commit

---
+++ RelationalOperators/src/relationaloperators/RelationalOperators.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package relationaloperators;
+
+/**
+ *
+ * @author T
+ * @coder: TMRM
+ * @project: Intro To Java - Comparing Things with Relational Operators (==, !=, <,>, <=, >=) Yielding T or F
+ * @version: 1.0 
+ * @since May 20, 2020
+ */
+public class RelationalOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //True Equal Operation
+        System.out.println(3==3);
+        //Fale Equal Operation
+        System.out.println(3==8);
+        //True Not Equal Operation
+        System.out.println(3!=69);
+        //Fale Not Equal Operation
+        System.out.println(3!=3);
+        //True Greater Than Operation
+        System.out.println(5>4);
+        //Flase Greater Than Operation
+        System.out.println(-1>4);
+        //True Less Than Operation
+        System.out.println(1<4);
+        //Flase Less Than Operation
+        System.out.println(6<4);
+        //True Less Than Or Equal Operation
+        System.out.println(1<4);
+        //Flase Less Than Or Equal Operation
+        System.out.println(6<4);
+        
+
+        
+        
+    }
+
+}
