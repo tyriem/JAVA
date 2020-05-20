dae39ea5d67d5c292f894fdbef9c97ea11643a9e
TMRM <tyrie.moss@gmail.com>
2020-05-19 22:13:11 -0400

    Initial commit

---
+++ Lab Exercise Variables/src/lab/exercise/variables/LabExerciseVariables.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab.exercise.variables;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intro To Java - Lab Exercise: Variables
+ * @version: 1.0
+ * @since: 05-14-2020
+ */
+public class LabExerciseVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    ///VARIABLE DECLARATIONS
+    //_Variables_
+    //Age Of Person
+    int agePerson = 29;
+        System.out.println (agePerson);
+           
+    //Single Letter
+    char letter = 't';
+       System.out.println (letter);
+       
+    //Price of an Apple
+    float apple = 1.44f;
+       System.out.print("$");
+       System.out.println(apple);
+       
+    //Color
+        String varName;
+    //Assign value to varAge_Float
+    varName = "Emerald";
+    //Print value in varAge_Float
+    System.out.println(varName); 
+    }
+
+}
