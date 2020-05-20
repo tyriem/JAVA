80ca87c8275a77bab632f1f0eccac0f78f9e3b9b
TMRM <tyrie.moss@gmail.com>
2020-05-20 16:06:50 -0400

    Initial commit

---
+++ UserInputRelOps/src/userinputrelops/UserInputRelOps.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package userinputrelops;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author T
+ * @coder: TMRM
+ * @project: Intro To Java - User Input & Evaluation of Relational Operators
+ * @version: 1.0 
+ * @since May 20, 2020
+ */
+public class UserInputRelOps {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);    
+  
+//Accept the Input of two values from the user
+
+//Ask the user to enter a number
+       System.out.println("Please Enter The First Number:");
+//Pull the user input into the memory buffer
+       double num1 = kb.nextInt();
+//Ask the user to enter a second number
+       System.out.println("Please Enter The Second Number:");
+//Pull the user input of the second number into the memory buffer
+       double num2 = kb.nextInt(); 
+  
+//Evaluate the numbers relationship with (num1 == num2)
+   
+//Print the buffer contents
+             System.out.print("The relationship " + num1 + " is equal to " + num2 + " is " + (num1 == num2));  
+     
+    }
+
+}
