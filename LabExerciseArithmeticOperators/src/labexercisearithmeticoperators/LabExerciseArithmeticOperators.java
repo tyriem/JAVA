3ca4a61f70586d2d6f04f05df8c765c64fdbed74
TMRM <tyrie.moss@gmail.com>
2020-05-19 22:13:34 -0400

    Initial commit

---
+++ LabExerciseArithmeticOperators/src/labexercisearithmeticoperators/LabExerciseArithmeticOperators.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package labexercisearithmeticoperators;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intro To Java - Arithmetic Operators
+ * @version: 1.1
+ * @since: 05-14-2020 - 05-19-2020
+ */
+public class LabExerciseArithmeticOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ ///Arithmetic Operators
+ //DECLARE AMOUNT OF STUDENTS OF EACH GENDER
+    double varFemale = 8;
+    double varMale = 13;
+ //ADD FOR CLASS AMOUNT
+       double varClass = varFemale + varMale;
+ //PRINT AMOUNT OF STUDENTS IN CLASS
+       System.out.println("Students: " + varClass);
+ //CALC PERCENTAGE OF EACH GENDER
+        double varPercFemale = ((varFemale / varClass)*100);
+        double varPercMale = ((varMale / varClass)*100);
+ //PRINT OUTPUT OF PERCENTAGES
+         System.out.print("Pecentage Female: " + Math.round(varPercFemale));
+         System.out.println("%");
+         System.out.print("Pecentage Male: " + Math.round(varPercMale));
+         System.out.println("%");
+    }
+
+}
