ffafa11518e250a4be6a2f799910ef0d9685a262
TMRM <tyrie.moss@gmail.com>
2020-05-26 15:58:05 -0400

    INITIAL COMMIT

---
+++ EvalEvensOrOdds/src/evalevensorodds/EvalEvensOrOdds.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package evalevensorodds;
+
+import java.util.Scanner;
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intro To Java - 
+ * @version: 1.0 
+ * @since May 26, 2020
+ */
+public class EvalEvensOrOdds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARE THE SCANNER
+         Scanner kb = new Scanner(System.in);
+        //
+        System.out.println("Enter Your Number Roll: ");
+        //PUT THE USER INPUT INTO THE MEM BUFFER
+        int num = kb.nextInt();    
+        //EVALUATE THE MODULUS FOR IF THE USER INPUT AN ODD OR AN EVEN
+        if (num == 0)
+           {
+            System.out.println("YOU ROLLED A ZERO (ORIGIN)!");
+           }
+        else if (num % 2 == 0)  
+           {
+              System.out.println("YOU ROLLED AN EVEN NUMBER!");
+           }
+        else
+           {
+              System.out.println("YOU ROLLED AN ODD NUMBER!");
+           } 
+                
+    }
+
+}
