30a972faf3a2f8eeb8891ba208391b974f46852b
TMRM <65630605+tyriem@users.noreply.github.com>
2020-05-27 16:12:09 -0400

    INITIAL COMMIT

---
+++ LogicalOperators/src/logicaloperators/LogicalOperators.java
@@ -0,0 +1,64 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package logicaloperators;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intro To Java - Logical Operators: AND (&&), || (OR), NOT (NOT)
+ * @version: 1.0 
+ * @since May 27, 2020
+ */
+public class LogicalOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // &&(AND) - Logical operator that connects two boolean expressions
+        // && works to check if BOTH expressions are true, if both are false or one is false it will evaluate as false
+        int temp = 2;
+        int time = 3;
+    if (temp < 5 && time < 6)
+       {
+    System.out.println("YOUR FRIDGE IS FINE! ");
+       }
+    else
+    {
+      System.out.println("SOMETHING IS WRONG WITH YOUR FRIDGE! "); 
+    } 
+      
+    
+        // ||(OR) - Logical operator that connects two boolean expressions
+        // || works to check if ONE OR BOTH expressions are true, if both are false  it will evaluate as false  
+        int speed = 2;
+        int rev = 3;
+    if (speed < 100 || rev < 100)
+       {
+    System.out.println("YOUR CAR IS FINE! ");
+       }
+    else
+       {
+      System.out.println("YOUR CAR IS STRESSED! "); 
+       }
+ 
+         // !(NOT) - Logical operator that connects two boolean expressions
+        // ! works to check if BOTH expressions are flase, if both are true or one is true it will evaluate as false
+        int heat = 100;
+        int humidity = 100;
+    if (heat < 100 ! humidity < 100)
+       {
+    System.out.println("YOUR CAR IS FINE! ");
+       }
+    else
+       {
+      System.out.println("YOUR CAR IS STRESSED! "); 
+       }
+
+    }
+}
