18e68a554db5a51fa39d2648fc6ce7fadf0d5874
TMRM <65630605+tyriem@users.noreply.github.com>
2020-05-27 01:52:25 -0400

    UPDATED JAVA FILES WITH NEW LICENSE FIELD

--- userinfo/src/userinfo/Userinfo.java
+++ userinfo/src/userinfo/Userinfo.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
  */
 
 package userinfo;
