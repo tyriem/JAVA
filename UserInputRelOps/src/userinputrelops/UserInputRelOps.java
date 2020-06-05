a30f6cfd9bcc4633a3ee01ca794c388c59638f2b
TMRM <65630605+tyriem@users.noreply.github.com>
2020-06-05 02:42:51 -0400

    UPDATED LOGIC

--- UserInputRelOps/src/userinputrelops/UserInputRelOps.java
+++ UserInputRelOps/src/userinputrelops/UserInputRelOps.java
@@ -38,8 +38,18 @@ public class UserInputRelOps {
 //Evaluate the numbers relationship with (num1 == num2)
    
 //Print the buffer contents
-             System.out.print("The relationship " + num1 + " is equal to " + num2 + " is " + (num1 == num2));  
-     
+             //Equals
+             System.out.print("\nThe relationship " + num1 + " is equal to " + num2 + " is " + (num1 == num2));  
+             //Not Equals
+             System.out.print("\nThe relationship " + num1 + " is not equal to " + num2 + " is " + (num1 != num2)); 
+             //Greater Than
+             System.out.print("\nThe relationship " + num1 + " is greater than " + num2 + " is " + (num1 > num2)); 
+             //Less Than
+             System.out.print("\nThe relationship " + num1 + " is less than " + num2 + " is " + (num1 < num2)); 
+             //Greater Than OR Equals
+             System.out.print("\nThe relationship " + num1 + " is greater than or equal to " + num2 + " is " + (num1 >= num2)); 
+             //Less Than OR Equals
+             System.out.print("\nThe relationship " + num1 + " is less than or equal " + num2 + " is " + (num1 <= num2));  
     }
 
 }
