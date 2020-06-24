8d917db418123f9e9a6eded4ef3c87ab3f7cc72b
TMRM <65630605+tyriem@users.noreply.github.com>
2020-06-24 02:15:58 -0400

    UPDATED COMMENTS

--- LabExerMethodDoubleIt/src/labexermethoddoubleit/LabExerMethodDoubleIt.java
+++ LabExerMethodDoubleIt/src/labexermethoddoubleit/LabExerMethodDoubleIt.java
@@ -8,6 +8,7 @@
  *[OBJECTIVE]
  * Create a method that doubles the value given and prints the result. 
  * Use this method from your Java program.
+ * NB: PAY ATTENTION TO WHAT IS ASKED OF YOUR METHOD CREATION, IN THIS CASE IT SAYS THE METHOD SHOULD PRINT THE OUTPUT SO IT DOESN'T NEED TO RETURN VALUE
  *[OBJECTIVE]
  */
 
@@ -19,7 +20,7 @@ package labexermethoddoubleit;
  *
  * @author T
  * @contributor: TMRM
- * @project: Intro To Java - Lab Exercise: Methods - Pass a Double It Method's Value
+ * @project: Intro To Java - Lab Exercise: Methods - Double It & Print Method
  * @version: 1.0 
  * @since Jun 22, 2020
  */
@@ -30,24 +31,28 @@ public class LabExerMethodDoubleIt {
      */
     public static void main(String[] args) {
         //TASK: METHOD TO DOUBLE A NUMBER
+        //Number var declaration
         int number = 3;
+        //Print statement to print numbers to be averaged
         System.out.println("Number is " + number);
-        //WHEN PASSING THE VALUE WE STORE IT IN A VAR HERE
-        //NB: By putting "number" below we copy the value of number into the method below
-        double newNumber = DubValue(number);
-        System.out.println("The double of " + number + " is: " + newNumber);
+        //Calling of method for averaging
+        DubValue(number);
+
 
     }
-    /**
-     * METHOD TO DOUBLE THE VALUE IN NUMBER
-     * @param number * 2
-     * @return newNumber
+    //COMMENTS FOR METHOD DECLARATION
+    /** 
+     * METHOD TO DOUBLE THE VALUE IN NUMBER AND PRINT THE NEW VALUE
+     * @param x * 2
      */
-    //IF WE WANT TO PASS A VALUE BACK WE SPECIFY THE VALUE TO RETURN INSTEAD OF VOID
-    public static double DubValue( double x)
+    //IF WE WANT TO PASS A VALUE BACK WE SPECIFY THE VALUE TO RETURN INSTEAD OF VOID, AS THERE IS NO RETURN HERE WE STILL USE VOID
+    //Set the method to the param of x as a Double
+    public static void DubValue( double x)
     {
-         double y = x * 2;
-         return y;
+        //Execute the doubling operation on x as y
+        double y = x * 2; 
+        //Print y
+        System.out.println("The double of the number is: " + y);
     }
 
 }
