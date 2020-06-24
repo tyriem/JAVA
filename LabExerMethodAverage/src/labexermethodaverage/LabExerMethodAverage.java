fdd959d7f8abf6d8c9d49e89f0e62edc17575351
TMRM <65630605+tyriem@users.noreply.github.com>
2020-06-24 02:16:08 -0400

    UPDATED COMMENTS

--- LabExerMethodAverage/src/labexermethodaverage/LabExerMethodAverage.java
+++ LabExerMethodAverage/src/labexermethodaverage/LabExerMethodAverage.java
@@ -8,6 +8,7 @@
  *[OBJECTIVE]
  * Write a method that accepts three decimal values (double or float), calculates the average of the three and prints this result.
  * Write code that tests your method. Include documentation comments.
+ * NB: PAY ATTENTION TO WHAT IS ASKED OF YOUR METHOD CREATION, IN THIS CASE IT SAYS THE METHOD SHOULD PRINT THE OUTPUT SO IT DOESN'T NEED TO RETURN VALUE
  *[OBJECTIVE]
  */
 
@@ -18,7 +19,7 @@ package labexermethodaverage;
  *
  * @author T
  * @contributor: TMRM
- * @project: Intro To Java - Lab Exercise: Method - Averaging
+ * @project: Intro To Java - Lab Exercise: Methods - Average Them & Print
  * @version: 1.0 
  * @since Jun 23, 2020
  */
@@ -29,30 +30,37 @@ public class LabExerMethodAverage {
      */
     public static void main(String[] args) {
     
-       //TASK: METHOD TO AVERAGE THREE NUMBERS
+        //TASK: METHOD TO AVERAGE THREE NUMBERS
+        //Number VAR declarations
         double numberOne = 3;
         double numberTwo = 4;
         double numberThree = 5;
-        System.out.println("Numbers are " + numberOne + " ," + numberTwo + " ," +numberThree + " ,");
-        //WHEN PASSING THE VALUE WE STORE IT IN A VAR HERE
+        //Print statement to print numbers to be averaged
+        System.out.println("Numbers are " + numberOne + " ," + numberTwo + " ," +numberThree + ".");
+        //Array to store the numbers
         double[] numArray ={numberOne, numberTwo, numberThree};
-        System.out.println("The average of the numbers is: " + AveValue(numArray) + " ,");
+        //Calling of method for averaging
+        aveValue(numArray);
+        
 
     }
     /**
      * METHOD TO AVERAGE THE VALUES IN double numArray[]
-     * @param (numberOne + numberTwo + numberThree)
-     * @return total/x.length
+     * @param (numberOne + numberTwo + numberThree)/3
      */
-    //IF WE WANT TO PASS A VALUE BACK WE SPECIFY THE VALUE TO RETURN INSTEAD OF VOID
-    public static double AveValue(double x[])
+    //IF WE WANT TO PASS A VALUE BACK WE SPECIFY THE VALUE TO RETURN INSTEAD OF VOID, AS THERE IS NO RETURN HERE WE STILL USE VOID
+    //Set the method to the param of x as a Double
+    public static void aveValue(double x[])
     {
+        //Declare Total VAR
         double total = 0;
+        //FOR LOOP To run through the array of numbers and adds them
         for(int i=0;i<x.length;i++) 
         {
         total += x[i];
         }
-        return total/x.length;
+        //PRINT STATEMENT TO Divide the total by the number of elements and print result
+        System.out.println("The Average of the numbers is: " + (total/x.length));
     }     
 
 }
