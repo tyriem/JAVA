b4118ccbbfd7c57068d9fbe2087b96ebfc7088d9
TMRM <tyrie.moss@gmail.com>
2020-09-10 08:20:09 -0400

    INITIAL COMMIT

---
+++ JIExerOperators/src/jiexeroperators/JIExerOperators.java
@@ -0,0 +1,41 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package jiexeroperators;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - 
+ * @version: 1.0 
+ * @since Sep 8, 2020
+ */
+public class JIExerOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Declare Static int variables
+    public static int a = 1;
+    public static int b = 3;
+    public static int c = 9;
+    public static int d = 27;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    //Perform operation on static ints and declare result variable   
+    int result = - a + b - c + d;
+    
+    //Print result variable
+    System.out.println(result);
+    }
+
+    
+}
