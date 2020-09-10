2db54b2041c12fe4e1658e15380437de0144d91f
TMRM <tyrie.moss@gmail.com>
2020-09-10 08:19:36 -0400

    INITIAL COMMIT

---
+++ JIExerPrint/src/jiexerprint/JIExerPrint.java
@@ -0,0 +1,34 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package jiexerprint;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Printing
+ * @version: 1.0 
+ * @since Sep 8, 2020
+ */
+public class JIExerPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int counterHello;
+        
+        //For-Loop Header
+        for(counterHello = 1; counterHello < 6; counterHello++)
+           {
+        //Print Statement       
+           System.out.println("Ellie is clever!");
+           } 
+
+    }
+
+}
