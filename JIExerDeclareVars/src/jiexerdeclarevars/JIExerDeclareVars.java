6f50431c8c33223bb64cb0efa74e451f0b8d3b8d
TMRM <tyrie.moss@gmail.com>
2020-09-10 08:19:43 -0400

    INITIAL COMMIT

---
+++ JIExerDeclareVars/src/jiexerdeclarevars/JIExerDeclareVars.java
@@ -0,0 +1,32 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package jiexerdeclarevars;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Declaring Variables
+ * @version: 1.0 
+ * @since Sep 8, 2020
+ */
+public class JIExerDeclareVars {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declare: String - name
+        String name;
+        //Declare: int - age
+        int age;
+        //Declare: String - city
+        String city;
+        
+    }
+
+}
