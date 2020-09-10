3cb851149802b700c8845ab0681711b0fe43cd9e
TMRM <tyrie.moss@gmail.com>
2020-09-10 08:19:55 -0400

    INITIAL COMMIT

---
+++ JIExerCommentingOut/src/jiexercommentingout/JIExerCommentingOut.java
@@ -0,0 +1,37 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package jiexercommentingout;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Commenting Out Code
+ * @version: 1.0 
+ * @since Sep 8, 2020
+ */
+public class JIExerCommentingOut {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+
+        //System.out.print("two");
+        System.out.print(b);
+        System.out.print(" plus ");
+        //System.out.print(" minus ");
+        System.out.print(a);
+        //System.out.print("three");
+        System.out.print(" is equal to ");
+        //System.out.print(" equals ");
+        //System.out.print("five");
+        System.out.print(a + b);
+    }
+
+}
