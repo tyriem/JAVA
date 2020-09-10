ba5df32df39d4202c15f672f90c062bd1d03756a
TMRM <tyrie.moss@gmail.com>
2020-09-10 08:20:01 -0400

    INITIAL COMMIT

---
+++ JIExerSquareNumber/src/jiexersquarenumber/JIExerSquareNumber.java
@@ -0,0 +1,31 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+package jiexersquarenumber;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Squaring a Number
+ * @version: 1.0 
+ * @since Sep 8, 2020
+ */
+public class JIExerSquareNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declare Variable Num
+        double num = 2;
+        //Declare and set logic for squaredNum (num*num)
+        double squaredNum = (num * num);
+        //Print squaredNum
+        System.out.print(squaredNum);
+    }
+
+}
