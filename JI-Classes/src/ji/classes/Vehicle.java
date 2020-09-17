e85598550e44a582f44d4b390dd526356764e9ae
TMRM <tyrie.moss@gmail.com>
2020-09-17 08:33:32 -0400

    UPDATED REFACTOR

---
+++ JI-Classes/src/ji/classes/Vehicle.java
@@ -0,0 +1,43 @@
+//NB: WHEN SETTING UP A CLASS OUTSIDE OF THE MAIN 
+//WE MUST SPECIFY THAT THE CLASS IS WITHIN THE SAME PACKAGE OR IT CANNOT BE REFERENCED
+package ji.classes;
+
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+/**
+ *
+ * @author T
+ */
+//Declare the Class
+public class Vehicle {
+//Declare the State variables of the Class
+    String type;
+    int wheels;
+    double engine;
+    String fuel;
+    int doors;
+    int windows;
+    int mirrors;
+    boolean brakes;
+    
+//Declare the Behaviour Variables of the Class   
+    void goForward(){
+        
+    }    
+    
+    void reverse(){    
+  
+    }
+    
+    void start(){    
+  
+    }
+    
+    void stop(){    
+  
+    }
+}
