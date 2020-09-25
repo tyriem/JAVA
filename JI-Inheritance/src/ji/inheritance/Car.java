a2324e4d48c22f652612112d1c4990ef89cffd80
TMRM <tyrie.moss@gmail.com>
2020-09-25 01:18:58 -0400

    INITIAL COMMIT

---
+++ JI-Inheritance/src/ji/inheritance/Car.java
@@ -0,0 +1,18 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+package ji.inheritance;
+
+/**
+ *
+ * @author T
+ */
+//SUBCLASS - Child Class
+class Car extends Vehicle {
+//NB: Private & Public are access modifiers
+// Java assumes Public if you don't specify
+  public String modelName = "Mustang";    // Car attribute
+  }
+
