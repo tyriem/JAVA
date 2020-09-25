66c35f8871e403e13b29b006b3e503adf5d1a68f
TMRM <tyrie.moss@gmail.com>
2020-09-25 01:18:50 -0400

    INITIAL COMMIT

---
+++ JI-Inheritance/src/ji/inheritance/Vehicle.java
@@ -0,0 +1,22 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+package ji.inheritance;
+
+/**
+ *
+ * @author T
+ */
+//SUPERCLASS - Parent Class
+class Vehicle {
+
+
+    
+//Instance Variable
+  protected String brand = "Ford";        // Vehicle attribute
+  public void honk() {                    // Vehicle method
+    System.out.println("Tuut, tuut!");
+  }
+}
