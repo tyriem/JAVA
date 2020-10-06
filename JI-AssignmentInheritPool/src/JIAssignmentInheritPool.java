8f00ef13077cfe84dde51b2f9f453a527214cb42
TMRM <tyrie.moss@gmail.com>
2020-10-06 08:33:45 -0400

    UPDATED

--- JI-AssignmentInheritPool/src/JIAssignmentInheritPool.java
+++ JI-AssignmentInheritPool/src/JIAssignmentInheritPool.java
@@ -58,23 +58,23 @@ public class JIAssignmentInheritPool {
        System.out.println("Please enter the height of the cuboid: (in meters)");
        double cubHei = kb.nextDouble();   
        
-       //CIRCLE OBJ: shape0_1
+       //Rectangle OBJ: shape0_1
        Rectangle shape0_1 = new Rectangle(recWid, recLen);
         
-       //CYLINDER OBJ: shape0_1
+       //Cuboid OBJ: shape0_1
        Cuboid shape0_2 = new Cuboid(recWid, recLen, cubHei);
        
        /// OUTPUT /// 
         System.out.println("-------------------------------------------------");
         System.out.println("[RECTANGLE INFORMATION]");
-        System.out.println("Rectangle Width: " + shape0_1.getWidth() + "cm");
-        System.out.println("Rectangle Length: " + shape0_1.getLength() + "cm");
-        System.out.println("Rectangle Area: " + shape0_1.getArea() + "cm^2");
+        System.out.println("Rectangle Width: " + shape0_1.getWidth() + "m");
+        System.out.println("Rectangle Length: " + shape0_1.getLength() + "m");
+        System.out.println("Rectangle Area: " + shape0_1.getArea() + "m^2");
         System.out.println("----------");
         System.out.println("[Cuboid INFORMATION]");
-        System.out.println("Cuboid Height: " + shape0_2.getHeight() + "cm");
-        System.out.println("Cuboid Area: " + shape0_2.getArea() + "cm");
-        System.out.println("Cuboid Volume: " + shape0_2.getVolume() + "cm^3");
+        System.out.println("Cuboid Height: " + shape0_2.getHeight() + "m");
+        System.out.println("Cuboid Area: " + shape0_2.getArea() + "m");
+        System.out.println("Cuboid Volume: " + shape0_2.getVolume() + "m^3");
         System.out.println("-------------------------------------------------");
         shape0_1.printDimRec();
         shape0_2.printDimCub();
