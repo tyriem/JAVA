3a5c998e5dca04a596b20aa73b0be1caca57fdc1
TMRM <tyrie.moss@gmail.com>
2020-10-27 11:24:29 -0400

    INITIAL COMMIT

---
+++ JI-ClassworkObjectLifetime/src/Cat.java
@@ -0,0 +1,24 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+/**
+ *
+ * @author T
+ * @project: Intermediate Java - Classwork: Object Lifetime
+ */
+
+/// Public Class: Cat ///
+public class Cat {
+    
+    /// PSVM ///
+    public static void main(){
+        
+    }
+    /// FINALIZE THE OBJECT ///
+    protected void finalize() throws Throwable{
+        
+    }
+}
