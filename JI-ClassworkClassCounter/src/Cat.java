54663e95717cb9b88a295bc6a15d46ac394655c1
TMRM <tyrie.moss@gmail.com>
2020-10-27 11:52:08 -0400

    INITIAL COMMIT

---
+++ JI-ClassworkClassCounter/src/Cat.java
@@ -0,0 +1,29 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+/**
+ *
+ * @author T
+ * @project: Intermediate Java - Classwork: Class Counter
+ */
+
+/// Public Class: Cat ///
+public class Cat {
+        // Init catCount
+    public static int catCount = 0;
+    
+        /// PSVM ///
+    public static void main(String[] args) {
+
+    }
+    
+    // Construct Class: Cat 
+    public Cat(){
+    // Increment catCount 
+        catCount++;
+    }
+    
+}
