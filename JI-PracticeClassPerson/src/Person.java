385bd3721c9e05394fe045c12552bada7627b94f
TMRM <tyrie.moss@gmail.com>
2020-10-27 10:19:33 -0400

    UPDATED COMMENTS

---
+++ JI-PracticeClassPerson/src/Person.java
@@ -0,0 +1,43 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+/*
+ * [OBJECTIVE]
+ * Create the Person Class. A person should have a String name, int age, String address, and char sex.
+ * Requirements:
+ * The program must not read data from the keyboard.
+ * The Person class must have a String variable name.
+ * The Person class must have an int variable age.
+ * The Person class must have a String variable address.
+ * The Person class must have char variable sex.
+ * [OBJECTIVE]
+ */
+
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Practice: Creating Classes - Person
+ * @version: 1.0 
+ * @since Oct 20, 2020
+ */
+
+
+// Define the CLASS
+/// CLASS: Person //
+public class Person {
+    
+    /// STATES ///
+    private String name;
+
+    private int age;
+    
+    private String address;
+    
+    private char sex;
+    
+}
