cac93769abc986040ce45eb20a02498715c473ef
TMRM <tyrie.moss@gmail.com>
2020-10-28 18:45:14 -0400

    INITIAL COMMIT

--- JI-ClassworkClassCounter/src/Cat.java
+++ JI-ClassworkClassCounter/src/Cat.java
@@ -4,6 +4,19 @@
  * [LICENSE INFORMATION]
  */
 
+/*
+ * [OBJECTIVE]
+ * Declare a static int variable catCount in the Cat class. Declare a constructor, i.e. public Cat(), that increases this variable by 1.
+ * Requirements:
+ * Add a constructor to the Cat class.
+ * The constructor must be public.
+ * Add the catCount field to the Cat class.
+ * The catCount field must be an int.
+ * The catCount field must be static.
+ * The constructor must increment the value of the variable catCount by 1.
+ * [OBJECTIVE]
+ */
+
 /**
  *
  * @author T
