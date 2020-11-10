7fb1b8821a8211b9452e0ef308c68be878572618
TMRM <tyrie.moss@gmail.com>
2020-11-10 02:29:29 -0500

    INITIAL COMMIT

---
+++ JI-AssignmentMinElement/src/ji/assignmentminelement/JIAssignmentMinElement.java
@@ -0,0 +1,37 @@
+/*
+ * [LICENSE INFORMATION]
+ * THIS WORK IS CURRENTLY NOT UNDER LICENSE, FOR MORE INFORMATION CONTACT ITS AUTHOR
+ * [LICENSE INFORMATION]
+ */
+
+/*
+ * [OBJECTIVE]
+ * Write a method called readIntegers() with a parameter called count that represents how many integers the user needs to enter.
+ * The method needs to read from the console until all the numbers are entered, and then return an array containing the numbers entered.
+ * Write a method findMin() with the array as a parameter. The method needs to return the minimum value in the array.
+ * In the main() method read the count from the console and call the method readIntegers() with the count parameter. 
+ * Then call the findMin() method passing the array returned from the call to the readIntegers() method.
+ * Finally, print the minimum element in the array.
+ * [OBJECTIVE]
+ */
+
+package ji.assignmentminelement;
+
+/**
+ *
+ * @author T
+ * @contributor: TMRM
+ * @project: Intermediate Java - Assignment: Minimum Element Challenge
+ * @version: 1.0 
+ * @since Nov 5, 2020
+ */
+public class JIAssignmentMinElement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
