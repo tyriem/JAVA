e85faca910818ff79136f0a63dafab6bcd43eee7
TMRM <tyrie.moss@gmail.com>
2020-11-10 02:29:48 -0500

    INITIAL COMMIT

--- JI-AssignmentFlipArray/src/ji/assignmentfliparray/JIAssignmentFlipArray.java
+++ JI-AssignmentFlipArray/src/ji/assignmentfliparray/JIAssignmentFlipArray.java
@@ -6,9 +6,14 @@
 
 /*
  * [OBJECTIVE]
- *  Run program below
- *  Test all of the available options
- *  Record the output
+ * Create an array of 10 numbers.
+ * Enter 10 numbers from the keyboard and write them to the array.
+ * Display the elements of the array in reverse order. Display each value on a new line.
+ * Requirements:
+ * The program must create an array of 10 integers.
+ * The program should read numbers for the array from the keyboard.
+ * The program should display 10 values, each on a new line.
+ * The array must be displayed in reverse order.
  * [OBJECTIVE]
  */
 
@@ -26,97 +31,44 @@ import java.util.Scanner;
  *
  * @author T
  * @contributor: TMRM
- * @project: Intermediate Java - Array List: Test
+ * @project: Intermediate Java - Array List: Flip Array
  * @version: 1.0 
  * @since Nov 5, 2020
  */
 public class JIAssignmentFlipArray {
     
-    // Start a new Scanner
-private static Scanner scanner = new Scanner(System.in);
-        
-private static GroceryList groceryList = new GroceryList();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-       boolean quit = false; 
-       int choice = 0; 
-       printInstructions(); 
-       while(!quit) { System.out.println("Enter your choice: " ); 
-            
-       choice = scanner.nextInt(); 
-       scanner.nextLine();     
-       
-       
-       switch(choice) { case 0: printInstructions(); 
-       break;
-       case 1: groceryList.printGroceryList(); 
-       break; 
-       case 2: addItem(); 
-       break; 
-       case 3: modifyItem(); 
-       break; 
-       case 4: removeItem();
-       break; 
-       case 5: searchForItem(); 
-       break; 
-       case 6: quit = true;
-       }
-    }
+           //Initialize Scanner
+           Scanner kb = new Scanner(System.in);  
+           //Declare VARs
+           final int ARRAY_SIZE_BCKWRDS = 10;
+           
+           //Create an array of integers
+           int[] numBack = new int[ARRAY_SIZE_BCKWRDS];
+           
+           //For Loop to Define the Array
+           for (int indexBck = 0; indexBck < ARRAY_SIZE_BCKWRDS; indexBck++)
+           {
+               //Ask the user for the value to put into the array element 
+               System.out.println("Enter Numbers For The Array: ");
+               numBack[indexBck] = kb.nextInt();
+           }
+           
+           System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+           
+           //Print the values in the array using enhanced FOR-LOOP
+           //NB: THE ENHANCED LOOP IS NOT USEFUL IN THIS CASE AS IT GOES INHERENTLY FORWARD
+           for (int indexBck = ARRAY_SIZE_BCKWRDS - 1; indexBck >= 0; indexBck--)
+           {
+               //Print the numbers
 
-}
-
-    
-    public static void printInstructions() {
-        System.out.println("\nPress ");
-        System.out.println("\t 0 - To print choice options.");
-        System.out.println("\t 1 - To print the list of grocery items.");
-        System.out.println("\t 2 - To add an item to the list.");
-        System.out.println("\t 3 - To modify an item in the list.");
-        System.out.println("\t 4 - To remove an item from the list.");
-        System.out.println("\t 5 - To search for an item in the list.");
-        System.out.println("\t 6 - To quit the application.");
-        }
-    
-    public static void addItem() { 
-        System.out.print("Please enter the grocery item: ");
-        groceryList.addGroceryItem(scanner.nextLine());
-    }
-    
-    public static void modifyItem() {
-        System.out.print("Enter item number: ");
-        int itemNo = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Enter replacement item: ");
-        String newItem = scanner.nextLine();
-        groceryList.modifyGroceryItem(itemNo-1,newItem);
-    }
-    
-    public static void removeItem() {
-        System.out.print("Enter item number: ");
-        int itemNo = scanner.nextInt();
-        scanner.nextLine();
-        groceryList.removeGroceryItem(itemNo-1);
-    }
-    
-    public static void searchForItem() {
-        System.out.print("Item to search for: ");
-        String searchItem = scanner.nextLine();
-        
-        if(groceryList.findItem(searchItem) != null)
-        {
-        System.out.println("Found " + searchItem + " in our grocery list");
-        }
-        else {
-        System.out.println(searchItem + " is not in the shopping list");
-        }
+           System.out.println("\n" + numBack[indexBck]);
+           
+           }
     }
 }
-
-    
-    
-////////////////////////////////////////////////////////
-
